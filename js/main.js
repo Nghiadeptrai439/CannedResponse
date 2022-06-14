0726f7eb1367a971ffa46712bc34b0903c7d8400
Newbie439 <creative.disaster99@gmail.com>
2022-06-14 14:29:00 +0700

    done

--- js/main.js
+++ js/main.js
@@ -18,14 +18,14 @@ $(function() {
     });
 });
 $(function() {
-    var b = $("#btnsetting");
+    var b = $("#btns");
     var w = $("#tab");
     var l = $("#list");
     
     w.width(l.outerWidth(true));
   
     b.click(function() {
-    
+      
       if(w.hasClass('midContent')) {
         w.removeClass('midContent');
         w.removeClass('mainContent__btnhide')
@@ -37,9 +37,11 @@ $(function() {
     
     });
 });
+
 function myFunction() {
 
   document.getElementById("button").style.display = "none";
+  document.getElementById("btns").classList.remove("active");
 }
 
 function addFunction() {
@@ -47,5 +49,15 @@ function addFunction() {
   // var element = document.getElementById("button");
   // element.classList.add("mainContent__btnhide");
   document.getElementById("button").style.display = "block";
+  document.getElementById("btns").classList.add("active");
+  
+
 }
+
+
+
+$('item--logo').on('click', function(){
+    $('item-logo').removClass('active');
+    $(this).addClass('active');
+});
 
